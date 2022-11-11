21ac036cb2ed14b9b04310263895952856101097
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-11-10 19:40:40 -0600

    :v:

--- mv.py
+++ mv.py
@@ -3,6 +3,7 @@ import random
 import pygame
 from OpenGL.GL import *
 from OpenGL.GL.shaders import *
+import glm
 
 pygame.init()
 
@@ -10,15 +11,25 @@ screen = pygame.display.set_mode(
     (1000, 800),
     pygame.OPENGL | pygame.DOUBLEBUF
 )
+# dT = pygame.time.Clock()
+
 
 
 vertex_shader = """
 #version 460
 layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 vertexColor;
+
+uniform mat4 amatrix;
+
+out vec3 ourColor;
+
 
 void main()
 {
-    gl_Position = vec4(position.x, position.y, position.z, 1.0f);
+    gl_Position = amatrix * vec4(position, 1.0f);
+    ourColor = vertexColor;
+
 }
 """
 
@@ -27,9 +38,15 @@ fragment_shader = """
 
 layout (location = 0) out vec4 fragColor;
 
+uniform vec3 color;
+
+
+in vec3 ourColor;
+
 void main()
 {
-    fragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    // fragColor = vec4(ourColor, 1.0f);
+    fragColor = vec4(color, 1.0f);
 }
 """
 compiled_vertex_shader = compileShader(vertex_shader, GL_VERTEX_SHADER)
@@ -38,13 +55,15 @@ shader = compileProgram(
     compiled_vertex_shader,
     compiled_fragment_shader
 )
+
 glUseProgram(shader)
 
 
+
 vertex_data = numpy.array([
-    -0.5, -0.5, 0.0,
-     0.5, -0.5, 0.0,
-     0.0,  0.5, 0.0,
+    -0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
+     0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
+     0.0,  0.5, 0.0, 0.0, 0.0, 1.0
 ], dtype=numpy.float32)
 
 vertex_buffer_object = glGenBuffers(1)
@@ -63,10 +82,54 @@ glVertexAttribPointer(
     3,
     GL_FLOAT,
     GL_FALSE,
-    3 * 4,
+    6 * 4,
     ctypes.c_void_p(0)
 )
 glEnableVertexAttribArray(0)
+
+
+glVertexAttribPointer(
+    1,
+    3,
+    GL_FLOAT,
+    GL_FALSE,
+    6 * 4,
+    ctypes.c_void_p(3 * 4)
+)
+glEnableVertexAttribArray(1)
+
+
+def calculateMatrix(angle):
+    i = glm.mat4(1)
+    translate = glm.translate(i, glm.vec3(0, 0, 0))
+    rotate = glm.rotate(i, glm.radians(angle), glm.vec3(0, 1, 0))
+    scale = glm.scale(i, glm.vec3(1, 1, 1))
+
+    model = translate * rotate * scale
+
+    view = glm.lookAt(
+        glm.vec3(0, 0, 5),
+        glm.vec3(0, 0, 0),
+        glm.vec3(0, 1, 0)
+    )
+
+    projection = glm.perspective(
+        glm.radians(45),
+        1000/800,
+        0.1,
+        1000.0
+    )
+
+    amatrix = projection * view * model
+
+    glUniformMatrix4fv(
+        glGetUniformLocation(shader, 'amatrix'),
+        1,
+        GL_FALSE,
+        glm.value_ptr(amatrix)
+    )
+
+glViewport(0, 0, 1000, 800)
 
 
 
@@ -74,8 +137,28 @@ running = True
 
 glClearColor(0.5, 1.0, 0.5, 1.0)
 
+r = 0
+
 while running:
+    r += 1
     glClear(GL_COLOR_BUFFER_BIT)
+
+    color1 = random.random()
+    color2 = random.random()
+    color3 = random.random()
+
+    color = glm.vec3(color1, color2, color3)
+
+    glUniform3fv(
+        glGetUniformLocation(shader,'color'),
+        1,
+        glm.value_ptr(color)
+    )
+
+    calculateMatrix(r)
+
+    pygame.time.wait(1)
+
 
     glDrawArrays(GL_TRIANGLES, 0, 3)
 
