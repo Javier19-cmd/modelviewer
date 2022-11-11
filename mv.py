79dd35f9ba37b5f0aa7c7ec4570e39736fda21b6
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-11-10 18:41:01 -0600

    cambios

--- mv.py
+++ mv.py
@@ -1,21 +1,86 @@
-import pygame 
+import numpy
 import random
+import pygame
+from OpenGL.GL import *
+from OpenGL.GL.shaders import *
 
 pygame.init()
 
-screen = pygame.display.set_mode((1000, 800))
+screen = pygame.display.set_mode(
+    (1000, 800),
+    pygame.OPENGL | pygame.DOUBLEBUF
+)
+
+
+vertex_shader = """
+#version 460
+layout (location = 0) in vec3 position;
+
+void main()
+{
+    gl_Position = vec4(position.x, position.y, position.z, 1.0f);
+}
+"""
+
+fragment_shader = """
+#version 460
+
+layout (location = 0) out vec4 fragColor;
+
+void main()
+{
+    fragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+}
+"""
+compiled_vertex_shader = compileShader(vertex_shader, GL_VERTEX_SHADER)
+compiled_fragment_shader = compileShader(fragment_shader, GL_FRAGMENT_SHADER)
+shader = compileProgram(
+    compiled_vertex_shader,
+    compiled_fragment_shader
+)
+glUseProgram(shader)
+
+
+vertex_data = numpy.array([
+    -0.5, -0.5, 0.0,
+     0.5, -0.5, 0.0,
+     0.0,  0.5, 0.0,
+], dtype=numpy.float32)
+
+vertex_buffer_object = glGenBuffers(1)
+glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object)
+glBufferData(
+    GL_ARRAY_BUFFER,  # tipo de datos
+    vertex_data.nbytes,  # tamaño de da data en bytes    
+    vertex_data, # puntero a la data
+    GL_STATIC_DRAW
+)
+vertex_array_object = glGenVertexArrays(1)
+glBindVertexArray(vertex_array_object)
+
+glVertexAttribPointer(
+    0,
+    3,
+    GL_FLOAT,
+    GL_FALSE,
+    3 * 4,
+    ctypes.c_void_p(0)
+)
+glEnableVertexAttribArray(0)
+
+
 
 running = True
 
+glClearColor(0.5, 1.0, 0.5, 1.0)
 
 while running:
-    x = random.randint(0, 1000)
-    y = random.randint(0, 1200)
-    screen.set_at((x, y), (255, 255, 255))
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    glDrawArrays(GL_TRIANGLES, 0, 3)
 
     pygame.display.flip()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
